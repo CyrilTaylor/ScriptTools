5c6706a70f7bef1a2f68f4be9b776ade9e7b4222
Cyril Taylor <CyrilTaylor@users.noreply.github.com>
2018-12-11 18:56:01 +0800

    Update .reposync.sh

--- .reposync.sh
+++ .reposync.sh
@@ -1,10 +1,19 @@
 function _wrap_build()
 {
     local start_time=$(date +"%s")
+    local start_ns=$(date +"%N")
     "$@"
     local ret=$?
+    local end_ns=$(date +"%N")
+    ((start_ns=10#$start_ns))
+    ((end_ns=10#$end_ns))
     local end_time=$(date +"%s")
+    if [ $start_ns -gt $end_ns ] ; then
+        end_time=$(($end_time-1))
+        end_ns=$(($end_ns+1000000000))
+    fi
     local tdiff=$(($end_time-$start_time))
+    local nsdiff=$((($end_ns-${start_ns}) / 10000000))
     local hours=$(($tdiff / 3600 ))
     local mins=$((($tdiff % 3600) / 60))
     local secs=$(($tdiff % 60))
@@ -28,8 +37,8 @@ function _wrap_build()
         printf "(%02g:%02g:%02g)" $hours $mins $secs
     elif [ $mins -gt 0 ] ; then
         printf "(%02g:%02g)" $mins $secs
-    elif [ $secs -gt 0 ] ; then
-        printf "(%s seconds)" $secs
+    elif [[ $secs -ne 0 || $nsdiff -ne 0 ]] ; then
+        printf "(%s.%02g seconds)" $secs $nsdiff
     fi
     echo " ---${color_reset}"
     echo
@@ -39,10 +48,11 @@ function _wrap_build()
 function reposync()
 {
     local ret=-1
-    while [ 0 != $ret ]; do
-    repo --trace sync --current-branch --no-clone-bundle --no-tags --prune
-    ret=$?
-    done
+    sleep 0.009
+    # while [ 0 != $ret ]; do
+    # repo --trace sync --current-branch --no-clone-bundle --no-tags --prune
+    # ret=$?
+    # done
 }
 
 _wrap_build reposync "$@"
